bd8947d6fee29230417a5c2ea6a76a4c17f099b5
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-02-11 23:57:44 -0200

    Aula 02.5 - Monkey Patch: grandes poderes trazem grandes responsabilidades

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,9 +1,10 @@
 var ConnectionFactory = (function () {
 
-    var stores = ['negociacoes'];
-    var version = 4;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
     var connection = null;
+    var close = null;
     
     return class ConnectionFactory {
 
@@ -23,6 +24,10 @@ var ConnectionFactory = (function () {
                 openRequest.onsuccess = e => {
                     if (!connection) {
                         connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar essa conexão');
+                        }
                     }
                     resolve(connection);
 
@@ -46,6 +51,14 @@ var ConnectionFactory = (function () {
                 }
                 connection.createObjectStore(store, { autoIncrement: true });
             });
+        }
+
+        static closeConnection() {
+
+            if(connection) {
+                close();
+                connection = null;
+            }
         }
     }
 })();
